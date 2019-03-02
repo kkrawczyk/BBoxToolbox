d12fb1a300bbe62ca2c817ec69532eb3c08b44a6
Krzysztof Krawczyk <krzysztof@krawczyk.czest.pl>
2019-03-02 10:31:16 +0100

    Bbox preview

--- biz/formats/XminXmaxYminYmaxFormat.py
+++ biz/formats/XminXmaxYminYmaxFormat.py
@@ -1,9 +1,34 @@
+import re
+from decimal import Decimal
+
+from qgis.core import QgsGeometry, QgsPointXY
+
 from .AbstractBBoxFormat import AbstractBBoxFormat
 
 
 class XminXmaxYminYmaxFormat(AbstractBBoxFormat):
+
+    def __init__(self):
+        self.pattern = r'\[([\d]+(\.\d+)?), ?([\d]+(\.\d+)?), ?([\d]+(\.\d+)?), ?([\d]+(\.\d+)?)\]'
+        self.compiledPattern = re.compile(self.pattern)
+
     def displayName(self):
         return "[Xmin, Xmax, Ymin, Ymax]"
 
     def format(self, bboxString):
         print(bboxString)
+        m = self.compiledPattern.match(bboxString)
+        if m is not None:
+            xmin = Decimal(m.group(1))
+            xmax = Decimal(m.group(3))
+            ymin = Decimal(m.group(5))
+            ymax = Decimal(m.group(7))
+            p1 = QgsPointXY(xmin, ymin)
+            p2 = QgsPointXY(xmax, ymin)
+            p3 = QgsPointXY(xmax, ymax)
+            p4 = QgsPointXY(xmin, ymax)
+            p5 = QgsPointXY(xmin, ymin)
+            coords = [p1, p2, p3, p4, p5]
+            geom = QgsGeometry.fromPolygonXY([coords])
+            return geom
+        return None
--- ui/BBoxPreviewWindow.py
+++ ui/BBoxPreviewWindow.py
@@ -3,8 +3,11 @@ import os
 import qgis
 import qgis.core
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import *
 
 from .. import biz
+
+B_BOX_TOOLBOX = "BBoxToolbox"
 
 try:
     from qgis.PyQt.QtGui import QDialog, QMessageBox
@@ -17,8 +20,9 @@ class BBoxPreviewWindow(QDialog):
         QDialog.__init__(self)
         ui_path = os.path.join(os.path.dirname(__file__), 'BBoxPreviewWindow.ui')
         uic.loadUi(ui_path, self)
-        self.bboxString.setPlainText(
-            '[20.09329832743496169, 21.92297676502402126, 52.71931350015093187, 51.73818035812892191]')
+        self.canvas = iface.mapCanvas()
+        self.destinationLayer.setCurrentIndex(0)
+        self.destinationLayer.setFilters(qgis.core.QgsMapLayerProxyModel.PolygonLayer)
         self.showButton.clicked.connect(self.showBbox)
         self.formatManager = biz.FormatManager()
         for format in self.formatManager.formatClasses:
@@ -32,9 +36,18 @@ class BBoxPreviewWindow(QDialog):
         format = self.formatManager.getFormatInstanceForClass(selectedFormat)
         self.displayBboxOnMap(layer, crs, bboxString, format)
 
-    def createTemporaryLayer(self):
-        # TODO: Create temporary layer
-        print('TODO: create temporary layer')
+    def createTemporaryLayer(self, crs):
+        typeString = "%s?crs=%s" % ('Polygon', crs.authid())
+        layerTitle = QCoreApplication.translate(B_BOX_TOOLBOX, "BBoxToolbox visualization")
+        layer = qgis.core.QgsVectorLayer(typeString, layerTitle, "memory")
+        layer.dataProvider().addAttributes([qgis.core.QgsField("name", QVariant.String)])
+        qgis.core.QgsProject.instance().addMapLayer(layer)
+        return layer
+
+    def createFeature(self, geom):
+        f = qgis.core.QgsFeature()
+        f.setGeometry(geom)
+        return f
 
     def show(self):
         self.formatManager.getAllFormats()
@@ -44,4 +57,14 @@ class BBoxPreviewWindow(QDialog):
 
     def displayBboxOnMap(self, dstLayer, crs, bboxString, formatInstance):
         bboxGeom = formatInstance.format(bboxString)
-        print(bboxGeom)
+        if dstLayer is None:
+            dstLayer = self.createTemporaryLayer(crs)
+        if dstLayer.readOnly():
+            errorMessage = QCoreApplication.translate(B_BOX_TOOLBOX, "Selected layer is readonly")
+            QMessageBox.Critical(self.iface.mainWindow(), B_BOX_TOOLBOX, errorMessage)
+            return
+        f = self.createFeature(bboxGeom)
+        dstLayer.dataProvider().addFeature(f)
+        dstLayer.updateExtents()
+        dstLayer.reload()
+        self.canvas.refreshAllLayers()
